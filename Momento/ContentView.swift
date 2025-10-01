e048870d2dd22b7e79d724c5d7b6c7103047ed46
haruna1256 <haru_kawagishi@icloud.com>
2025-10-01 09:23:04 +0900

    Initial Commit

---
+++ Momento/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Momento
+//
+//  Created by 川岸遥奈 on 2025/10/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Momento/MomentoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentoApp.swift
+//  Momento
+//
+//  Created by 川岸遥奈 on 2025/10/01.
+//
+
+import SwiftUI
+
+@main
+struct MomentoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
