c2470e1024a4b1f511bdf4c943c0215b95d689e2
haruna1256 <haru_kawagishi@icloud.com>
2025-10-02 14:57:31 +0900

    [add] 画面遷移のタブ要素の追加

---
+++ Momento/Screens/Tab/TabItem.swift
@@ -0,0 +1,14 @@
+//
+//  TabItem.swift
+//  Momento
+//
+//  Created by 川岸遥奈 on 2025/10/02.
+//
+
+// 画面遷移のタブ要素の定義
+enum TabItem: CaseIterable {
+    case home
+    case map
+    case notification
+    case setting
+}
