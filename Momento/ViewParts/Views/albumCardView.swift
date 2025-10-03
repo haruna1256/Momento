dcdd000e691d3a028633c1ff6c5c61b9a1f2a84a
haruna1256 <haru_kawagishi@icloud.com>
2025-10-03 10:25:38 +0900

    [add] 新規ファイルの作成

---
+++ Momento/ViewParts/Views/albumCardView.swift
@@ -0,0 +1,18 @@
+//
+//  albumCardView.swift
+//  Momento
+//
+//  Created by 川岸遥奈 on 2025/10/03.
+//
+
+import SwiftUI
+
+struct albumCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    albumCardView()
+}
