858a69c70791ac61c2fa3129f94554634d882dbb
haruna1256 <haru_kawagishi@icloud.com>
2025-10-13 11:04:27 +0900

    [add] マップでの現在地の表示

--- Momento/ViewParts/Map/LocationManager.swift
+++ Momento/ViewParts/Map/LocationManager.swift
@@ -3,19 +3,74 @@
 //  Momento
 //
 //  Created by 川岸遥奈 on 2025/10/09.
-//
 
+
+import MapKit
 import CoreLocation
+import Combine
 
-class LocationManager: NSObject, CLLocationManagerDelegate {
+// 位置情報のアノテーションを表す構造体
+struct LocationAnnotation: Identifiable {
+    let id = UUID()
+    let coordinate: CLLocationCoordinate2D
+}
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     let locationManager = CLLocationManager()
+
+    @Published private(set) var annotations: [LocationAnnotation] = []
+
+    // 地図の表示領域
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 35.6895, longitude: 139.6917), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+
+    // 重要: regionが更新されたことを通知するためのEquatableなID
+    @Published var regionUpdateID = UUID()
 
     override init() {
         super.init()
         locationManager.delegate = self
+
+        // desiredAccuracy の設定は、init の完了を待ってから実行する。
+        DispatchQueue.main.async {
+             self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        }
     }
 
-    func requestLocationAuthorization() {
+    func requestLocation() {
         locationManager.requestWhenInUseAuthorization()
+        // startUpdatingLocation() の呼び出しは権限付与後に delegate メソッドで行う。
+    }
+
+    // 位置情報が更新されたときに呼ばれるdelegateメソッド
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+
+        // Publishing changesエラー対策: メインスレッドの次のサイクルで実行
+        DispatchQueue.main.async {
+            let center = location.coordinate
+            let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+
+            // 1. annotations の更新
+            self.annotations = [LocationAnnotation(coordinate: center)]
+
+            // 2. region と regionUpdateID の更新
+            self.region = MKCoordinateRegion(center: center, span: span)
+            self.regionUpdateID = UUID() // IDを更新してビューに通知
+
+            // 取得成功後、停止する
+            manager.stopUpdatingLocation()
+        }
+    }
+
+    // 権限ステータスが変更されたときに呼ばれるdelegateメソッド
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            manager.startUpdatingLocation() // 権限付与後に更新開始
+        case .denied, .restricted, .notDetermined:
+            break
+        @unknown default:
+            break
+        }
     }
 }
--- Momento/ViewParts/Map/mapStyleView.swift
+++ Momento/ViewParts/Map/mapStyleView.swift
@@ -9,31 +9,45 @@ import MapKit
 
 // マップを表示する
 struct mapStyleView: View {
-    let locationManager = LocationManager()
+    @StateObject private var locationManager = LocationManager()
+
+    // MapCameraPositionで地図のカメラ位置を管理 (新しいAPI)
+    @State private var position: MapCameraPosition = .automatic
+
     var body: some View {
-        Map(interactionModes: .all) {
-                    UserAnnotation(anchor: .center) { userLocation in
-                        VStack {
-                            Image(systemName: "arrow.up")
-                                .rotationEffect(.degrees(userLocation.heading?.magneticHeading ?? 0))
-                                .foregroundColor(.blue)
-                            Circle()
-                                .foregroundStyle(.blue)
-                                .padding(2)
-                                .background(
-                                    Circle()
-                                        .fill(.white)
-                                )
-                            Text("me")
-                        }
+
+        // Mapを新しい Map(position:...) 初期化子に変更
+        Map(position: $position) {
+
+            // アノテーションの表示
+            ForEach(locationManager.annotations) { annotation in
+                Annotation("現在地", coordinate: annotation.coordinate) {
+                    VStack {
+                        Image(systemName: "mappin.circle.fill")
+                            .foregroundColor(.red)
+                            .font(.title)
+
                     }
                 }
-                .mapControls {
-                    MapUserLocationButton()
-                }
-                .onAppear {
-                    locationManager.requestLocationAuthorization()
-                }
+            }
+
+            // ユーザーの位置を示す
+            UserAnnotation()
+        }
+        .mapControls {
+            MapUserLocationButton()
+        }
+        .onAppear {
+            locationManager.requestLocation()
+            // 初期位置を LocationManager の初期 region で設定
+            position = .region(locationManager.region)
+        }
+
+        // 重要: Equatableな regionUpdateID の変更を監視する
+        .onChange(of: locationManager.regionUpdateID) { _ in
+            // IDが変わったら、最新の region に基づいて position を更新
+            position = .region(locationManager.region)
+        }
     }
 }
 
